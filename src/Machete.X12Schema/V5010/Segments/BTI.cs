ae882ddbb5332bca3dfa6d8c81b7e27cc2222fbe
Dominique Plante <dominique.plante@gmail.com>
2020-08-30 14:01:35 -0700

    Fix naming convention and data type conversion for BTI segment (5010 segment)

--- src/Machete.X12Schema/V5010/Segments/BTI.cs
+++ src/Machete.X12Schema/V5010/Segments/BTI.cs
@@ -11,25 +11,25 @@ namespace Machete.X12Schema.V5010
 
         Value<string> ReferenceIdentification { get; }
 
-        Value<string> IdentifierCodeQualifier03 { get; }
+        Value<string> IdentifierCodeQualifier1 { get; }
 
-        Value<string> IdentifierCode04 { get; }
+        Value<string> IdentifierCode1 { get; }
 
         Value<DateTime> Date { get; }
 
         Value<string> NameCodeIdentifier { get; }
 
-        Value<string> IdentifierCodeQualifier07 { get; }
+        Value<string> IdentifierCodeQualifier2 { get; }
 
-        Value<string> IdentifierCode08 { get; }
+        Value<string> IdentifierCode2 { get; }
 
-        Value<string> IdentifierCodeQualifier09 { get; }
+        Value<string> IdentifierCodeQualifier3 { get; }
 
-        Value<string> IdentifierCode10 { get; }
+        Value<string> IdentifierCode3 { get; }
 
-        Value<string> IdentifierCodeQualifier11 { get; }
+        Value<string> IdentifierCodeQualifier4 { get; }
 
-        Value<string> IdentifierCode12 { get; }
+        Value<string> IdentifierCode4 { get; }
 
         Value<string> TransactionSetPurposeCode { get; }
 
--- src/Machete.X12Schema/V5010/Segments/Maps/BTIMap.cs
+++ src/Machete.X12Schema/V5010/Segments/Maps/BTIMap.cs
@@ -2,6 +2,7 @@ namespace Machete.X12Schema.V5010.Maps
 {
     using X12;
     using X12.Configuration;
+    using X12.Values.Converters;
 
 
     public class BTIMap :
@@ -14,16 +15,20 @@ namespace Machete.X12Schema.V5010.Maps
 
             Value(x => x.ReferenceIdentificationQualifier, 1, x=> x.MinLength(2).MaxLength(3));
             Value(x => x.ReferenceIdentification, 2, x=> x.MinLength(1).MaxLength(50).IsRequired());
-            Value(x => x.IdentifierCodeQualifier03, 3, x=> x.MinLength(1).MaxLength(2));
-            Value(x => x.IdentifierCode04, 4, x=> x.MinLength(2).MaxLength(80).IsRequired());
-            Value(x => x.Date, 5, x=> x.FixedLength(8));
+            Value(x => x.IdentifierCodeQualifier1, 3, x=> x.MinLength(1).MaxLength(2));
+            Value(x => x.IdentifierCode1, 4, x=> x.MinLength(2).MaxLength(80).IsRequired());
+            Value(x => x.Date, 5, x =>
+            {
+                x.FixedLength(8);
+                x.Converter = X12ValueConverters.LongDate;
+            });
             Value(x => x.NameCodeIdentifier, 6, x=> x.MinLength(1).MaxLength(4));
-            Value(x => x.IdentifierCodeQualifier07, 7, x=> x.MinLength(1).MaxLength(2));
-            Value(x => x.IdentifierCode08, 8, x=> x.MinLength(2).MaxLength(80));
-            Value(x => x.IdentifierCodeQualifier09, 9, x=> x.MinLength(1).MaxLength(2));
-            Value(x => x.IdentifierCode10, 10, x=> x.MinLength(2).MaxLength(80));
-            Value(x => x.IdentifierCodeQualifier11, 11, x=> x.MinLength(1).MaxLength(2));
-            Value(x => x.IdentifierCode12, 12, x=> x.MinLength(2).MaxLength(80));
+            Value(x => x.IdentifierCodeQualifier2, 7, x=> x.MinLength(1).MaxLength(2));
+            Value(x => x.IdentifierCode2, 8, x=> x.MinLength(2).MaxLength(80));
+            Value(x => x.IdentifierCodeQualifier3, 9, x=> x.MinLength(1).MaxLength(2));
+            Value(x => x.IdentifierCode3, 10, x=> x.MinLength(2).MaxLength(80));
+            Value(x => x.IdentifierCodeQualifier4, 11, x=> x.MinLength(1).MaxLength(2));
+            Value(x => x.IdentifierCode4, 12, x=> x.MinLength(2).MaxLength(80));
             Value(x => x.TransactionSetPurposeCode, 13, x=> x.FixedLength(2));
             Value(x => x.TransactionTypeCode, 14, x=> x.FixedLength(2));
         }
