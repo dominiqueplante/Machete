6cc2b3aaa758eb1868a8d4cf7a7d79df8a638068
Albert Hives <ahives@hotmail.com>
2020-09-01 08:06:23 -0700

    Fixed X12 300 transaction set mapper

--- src/Machete.X12Schema/V5010/Layouts/Maps/T300Map.cs
+++ src/Machete.X12Schema/V5010/Layouts/Maps/T300Map.cs
@@ -10,7 +10,7 @@ namespace Machete.X12Schema.V5010.Maps
         public T300Map()
         {
             Id = "T300";
-            Name = "858 Shipment Information";
+            Name = "300 Reservation (Booking Request) (Ocean)";
             
             Segment(x => x.FunctionalGroupHeader, 0);
             Segment(x => x.TransactionSetHeader, 1);
@@ -18,21 +18,21 @@ namespace Machete.X12Schema.V5010.Maps
             Segment(x => x.Contact, 3);
             Segment(x => x.Authentication, 4);
             Segment(x => x.CargoBookingPriority, 5);
-            Segment(x => x.SpaceReservationRequest, 5);
-            Layout(x => x.LoopY2, 6);
-            Segment(x => x.ExtendedReferenceInformation, 7);
-            Segment(x => x.RouteInformationWithPreference, 8);
-            Layout(x => x.LoopN1, 9);
-            Layout(x => x.LoopR4, 10);
-            Segment(x => x.EquipmentAndTemperature, 11);
-            Segment(x => x.SpecialHandlingInstructions, 12);
-            Segment(x => x.EquipmentAttributes, 13);
-            Layout(x => x.LoopLX, 14);
-            Segment(x => x.VesselIdentification, 15);
-            Segment(x => x.EventDetail, 16);
-            Segment(x => x.Remarks, 17);
-            Segment(x => x.TransactionSetTrailer, 38);
-            Segment(x => x.FunctionalGroupTrailer, 39);
+            Segment(x => x.SpaceReservationRequest, 6);
+            Layout(x => x.LoopY2, 7);
+            Segment(x => x.ExtendedReferenceInformation, 8);
+            Segment(x => x.RouteInformationWithPreference, 9);
+            Layout(x => x.LoopN1, 10);
+            Layout(x => x.LoopR4, 11);
+            Segment(x => x.EquipmentAndTemperature, 12);
+            Segment(x => x.SpecialHandlingInstructions, 13);
+            Segment(x => x.EquipmentAttributes, 14);
+            Layout(x => x.LoopLX, 15);
+            Segment(x => x.VesselIdentification, 16);
+            Segment(x => x.EventDetail, 17);
+            Segment(x => x.Remarks, 18);
+            Segment(x => x.TransactionSetTrailer, 19);
+            Segment(x => x.FunctionalGroupTrailer, 20);
         }
     }
 }
